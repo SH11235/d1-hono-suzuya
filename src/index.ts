f8da5401b603bc9548d5fa30f9690c42f4cfdad5
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2023-12-30 03:36:05 +0900

    rename

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 
 import { Bindings } from "./types";
 import { corsMiddleware } from "./middlewares/cors";
-import { adminRoutes } from "./routes/adminRoutes";
+import { adminRoutes } from "./routes/admin";
 import { authMiddleware } from "./middlewares/auth";
 
 const app = new Hono<{ Bindings: Bindings }>();
