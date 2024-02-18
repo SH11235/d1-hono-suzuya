c998cdeae8c6644d9847bced5d2ddafff6ef3fbc
SH11235 <aabceh1a1a2i3u5o8@gmail.com>
2024-02-18 16:40:20 +0900

    ログインフォームcomponentのパス変更

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Toaster } from "react-hot-toast";
-import { LoginForm } from "./loginForm";
+import { LoginForm } from "../components/loginForm";
 import { JwtProvider } from "@/context/CredentialProvider";
 
 const LoginPage = () => {
